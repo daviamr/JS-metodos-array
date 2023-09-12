013a4e69be530a00aa5d72b0be92ea8c32354ebe
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-12 03:01:00 -0300

    refactor-valor-total

--- js/api_interaction.js
+++ js/api_interaction.js
@@ -1,19 +1,17 @@
 const books_box_content = document.getElementById('livros');
 const byPrice = document.getElementById('btnOrdenarPorPreco');
 const buttons = document.querySelectorAll('.btn');
-
 const totalValueElement = document.getElementById('valor_total_livros_disponiveis');
 
-
-//Função para mostrar os livros na página;
-function showBooks(booksData) { //Primeira vez chamada no main.js, executa e mostra todos os livros;
-    books_box_content.innerHTML = ''; //Quando um botão é clicado, ele reseta os livros e chama a função novamente com a array da categoria do botao clicado como parâmetro
+//função para mostrar os livros na página;
+function showBooks(booksData) { //primeira vez chamada no main.js, executa e mostra todos os livros;
+    books_box_content.innerHTML = '';
     booksData.forEach((book) => {
-        //Condicional aplicando a classe indisponivel caso a quantidade seja >= 0;
+        //condicional aplicando a classe indisponivel caso a quantidade seja >= 0;
         let disponibility = book.quantidade > 0
             ? 'livro__imagens'
             : 'livro__imagens indisponivel';
-        //Condicional aplicando o valor 0 à livros indisponíveis, para não atrapalhar a devolução do valor final no reduce() na func displayTotalAmount();
+        //condicional aplicando o valor 0 à livros indisponíveis, para não atrapalhar a devolução do valor final no reduce() na func displayTotalAmount();
         let value = book.quantidade == 0
             ? book.preco = 0
             : book.preco;
@@ -32,19 +30,19 @@ function showBooks(booksData) { //Primeira vez chamada no main.js, executa e mos
     })
 }
 
-//Evento adicionado no array que contém todos os botões;
+//evento adicionado no array que contém todos os botões;
 buttons.forEach(button => button.addEventListener('click', booksFilter));
 
-function booksFilter() { //Quando chamada
-    const button = document.getElementById(this.id); //Verifica qual o id do botão clicado;
-    const category = button.value; //Obtém o valor atributo 'value' do botão clicado;
+function booksFilter() { //quando chamada
+    const button = document.getElementById(this.id); //verifica qual o id do botão clicado;
+    const category = button.value; //obtém o valor atributo 'value' do botão clicado;
 
     filteredBooks = category == 'disponivel'
         ? booksData.filter(book => book.quantidade > 0)
         : booksData.filter(book => book.categoria == category);
 
-    showBooks(filteredBooks); //Mostra os livros filtrados;
-    displayTotalAmount(filteredBooks); //Mostra o valor total dos livros filtrados;
+    showBooks(filteredBooks); //mostra os livros filtrados;
+    displayTotalAmount(filteredBooks); //mostra o valor total dos livros filtrados;
 }
 
 //evento click chamando a função de ordenação;
@@ -54,13 +52,13 @@ byPrice.addEventListener('click', orderByPrice);
 function orderByPrice() {
     let orderedBooks = booksData.sort((a, b) => a.preco - b.preco);
     showBooks(orderedBooks);
-    displayTotalAmount(booksData);
+    displayTotalAmount(orderedBooks);
 }
 
 //função mostrando o valor total dos livros disponíveis;
 function displayTotalAmount(booksData) {
     totalValue = booksData.reduce((acc, book) => acc + book.preco, 0);
-    //booksData.reduce(acumulador, livro) acumulador + valor livro, acumulador começando com 0 e acumulando o valor de cada livro;
+    //booksData.reduce(acumulador, livro) acumulador + valor livro, acumulador começando com 0 e segue acumulando o valor de cada livro;
     //reduce() basicamente reduz todo um array em um único valor final;
 
     totalValueElement.innerHTML =
@@ -72,9 +70,9 @@ function displayTotalAmount(booksData) {
         </div>`;
 }
 
-//Função para aplicar um desconto dinamicamente;
+//função para aplicar um desconto dinamicamente;
 function applyDiscount(books) {
-    const discount = 0.3;
+    const discount = 0.3; //em % (1 = 100%)
 
     booksDataWithDicount = books.map(book => {
         return { ...book, preco: book.preco - (book.preco * discount) }
